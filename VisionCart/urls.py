f39ea5e5967f2b3ebca87b4341a49c132670ed53
Rahul <rahulmsuresh70@gmail.com>
2026-09-20 17:59:04 +0530

    Serve media files in production

--- VisionCart/urls.py
+++ VisionCart/urls.py
@@ -1,11 +1,11 @@
 ## =========================================================
 ## VISIONCART MAIN URL CONFIGURATION
 ## =========================================================
-
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.views.static import serve
+from django.urls import include, path, re_path
 
 
 ## =========================================================
@@ -44,9 +44,13 @@ urlpatterns = [
 ## MEDIA FILES
 ## =========================================================
 
-if settings.DEBUG:
 
-    urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
-)
+urlpatterns += [
+    re_path(
+        r"^media/(?P<path>.*)$",
+        serve,
+        {
+            "document_root": settings.MEDIA_ROOT,
+        },
+    ),
+]
